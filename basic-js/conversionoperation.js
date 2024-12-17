5bd3071e8a5c12154e6b491983ecd8980f54275f
kittuRR <r2103kittu@gmail.com>
2024-12-17 11:39:01 +0000

    conversion of datatypes

---
+++ basic-js/conversionoperation.js
@@ -0,0 +1,35 @@
+let id = 35
+let name = "Shital"
+let pswd = "abc123"
+
+console.log(typeof id);
+console.log(typeof name);
+console.log(typeof (pswd));
+
+let valueInNumber = Number(id)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "35" => 35
+// "35abc" => NaN
+// true => 1; false => 0
+
+let logged = 1
+let isLoggedIn = Boolean(logged)
+console.log(typeof isLoggedIn);
+console.log(isLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber = 50
+let isString = String(someNumber)
+console.log(typeof isString);
+console.log(isString);
+
+
+
+
+
+
