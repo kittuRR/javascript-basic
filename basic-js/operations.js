0c2e8e078f83956d8704348a2c1e020177e06161
kittuRR <r2103kittu@gmail.com>
2024-12-30 10:41:31 +0000

    conversion-operations-checked

---
+++ basic-js/operations.js
@@ -0,0 +1,23 @@
+let value = 5
+let negValue = -value
+
+// Arithmetic operations
+
+console.log(negValue);
+console.log(15 + 5);
+console.log(20 - 10);
+console.log(3 * 5);
+console.log(16 / 4);
+console.log(3 ** 3);
+console.log(100 % 45);
+
+
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+
+
+
+
+
+
